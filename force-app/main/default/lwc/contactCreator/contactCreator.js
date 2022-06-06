cc4e6c4711bd837f17001bd45a5439df93134792
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-06-06 15:34:18 -0400

    3rd

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -3,7 +3,7 @@ import CON_OBJ from '@salesforce/schema/Contact';
 import CON_FST_NAME from '@salesforce/schema/Contact.FirstName';
 import CON_LST_NAME from '@salesforce/schema/Contact.LastName';
 import CON_EMAIL from '@salesforce/schema/Contact.Email';
-import ShowToastEvent from 'lightning/ShowToastEvent';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 export default class ContactCreator extends LightningElement {
     fields=[CON_FST_NAME, CON_LST_NAME, CON_EMAIL];
     objectApiName = CON_OBJ.objectApiName;
