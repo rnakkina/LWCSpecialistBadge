70db91631c9b78f09db57f63f918dcd2618d2293
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-06-06 16:58:40 -0400

    4th comit

---
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -0,0 +1,16 @@
+import { LightningElement, wire } from 'lwc';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+import CON_FST_NAME from '@salesforce/schema/Contact.FirstName';
+import CON_LST_NAME from '@salesforce/schema/Contact.LastName';
+import CON_EMAIL from '@salesforce/schema/Contact.Email';
+const COLUMNS = [
+    { label: 'Contact First Name', fieldName: CON_FST_NAME.fieldApiName, type: 'text' },
+    { label: 'Contact Last Name', fieldName: CON_LST_NAME.fieldApiName, type: 'text' },
+    { label: 'Contact Email', fieldName: CON_EMAIL.fieldApiName, type: 'email' }
+];
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts
+
+}
