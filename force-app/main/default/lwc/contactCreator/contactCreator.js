1f5eb2a7ca52fa37643c7e441d3ee761b0e40138
ramakrishna.nakkina <ramakrishna.nakkina@gmail.com>
2022-06-06 15:21:00 -0400

    contactCreator initial commit

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -1,3 +1,19 @@
 import { LightningElement } from 'lwc';
+import {CON_OBJ} from '@salesforce/schema/Contact';
+import {CON_FST_NAME} from '@salesforce/schema/Contact.FirstName';
+import {CON_LST_NAME} from '@salesforce/schema/Contact.LastName';
+import {CON_EMAIL} from '@salesforce/schema/Contact.Email';
+import {ShowToastEvent} from 'lightning/ShowToastEvent';
+export default class ContactCreator extends LightningElement {
+    fields=[CON_FST_NAME, CON_LST_NAME, CON_EMAIL];
+    objectApiName = CON_OBJ.objectApiName;
+    handleSuccess(event){
+        const toastEvent = new ShowToastEvent({
+            title: "Contact created",
+            message: "Record ID: " + event.detail.id,
+            variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+    }
 
-export default class ContactCreator extends LightningElement {}
+}
